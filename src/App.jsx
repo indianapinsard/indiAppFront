ef7cbab4564d876057c10ddfa02e5ced3af580e5
Indi <indiana.pinsard@wattys-conseil.com>
2021-03-02 15:37:18 +0100

    add request to api

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-/**
- * Build a button that says clicked when clicked.
- * @return {HTMLButtonElement} The html code for the button.
- */
-function App() {
-  const [state, setState] = useState('CLICK ME');
-  return <button type="button" onClick={() => setState('CLICKED')}>{state}</button>;
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      label: 'CLICK ME',
+      list: [],
+    };
+  }
+
+  getDataFromApi = () => {
+    const API_ENDPOINT = (process.env.NODE_ENV == 'development') 
+      ? 'http://127.0.0.1:8000/article-list/' 
+      : 'https://api.wattplanner.ovh/article-list/';
+
+    axios({
+      method: 'get',
+      url: API_ENDPOINT,
+    }).then(function (response) {
+        this.setState({list: response.data});
+        this.setState({label: 'CLICKED'});
+      }.bind(this));
+  }
+
+  render() {
+    return(
+      <div>
+        <div>{this.state.list.map((e, i) => <li key={i}>{e.link}</li>)}</div>
+        <button type="button" onClick={this.getDataFromApi}>{this.state.label}</button>
+      </div>
+    )
+  }
 }
 
 export default App;
