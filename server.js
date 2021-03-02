250578478dc945ec58517be5bef15a28e07541cf
Indi <indiana.pinsard@wattys-conseil.com>
2021-03-02 16:22:50 +0100

    add htpps redirect

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.all('*', ensureSecure);
+
 app.use(express.static(path.join(__dirname, 'build')));
+
+app.listen(port, () => `App is live on port ${port}!`);
 
 function ensureSecure(req, res, next) {
   if (req.header('X-Forwarded-Proto') === 'https') {
     next();
   } else {
-    res.redirect(`https://${req.host}${req.url}`);
+    res.redirect(`https://${req.hostname}${req.url}`);
   }
 }
-
-app.all('*', ensureSecure);
-
-app.listen(port, () => `App is live on port ${port}!`);
