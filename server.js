766cd42271a9cb473b66806b386f9f9def54fb23
Indi <indiana.pinsard@wattys-conseil.com>
2021-03-02 16:06:57 +0100

    add redirection to https

--- server.js
+++ server.js
@@ -6,4 +6,14 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+function ensureSecure(req, res, next){
+  if (req.header('X-Forwarded-Proto') == 'https') {
+    return next();
+  } else {
+    res.redirect('https://' + req.host + req.url);
+  }
+};
+
+app.all('*', ensureSecure);
+
 app.listen(port, () => `App is live on port ${port}!`);
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-
 class App extends Component {
-
   constructor() {
     super();
     this.state = {
@@ -12,27 +10,28 @@ class App extends Component {
     };
   }
 
-  getDataFromApi = () => {
-    const API_ENDPOINT = (process.env.NODE_ENV == 'development') 
-      ? 'http://127.0.0.1:8000/article-list/' 
+  getDataFromApi() {
+    const API_ENDPOINT = (process.env.NODE_ENV === 'development')
+      ? 'http://127.0.0.1:8000/article-list/'
       : 'https://api.wattplanner.ovh/article-list/';
 
     axios({
       method: 'get',
       url: API_ENDPOINT,
-    }).then(function (response) {
-        this.setState({list: response.data});
-        this.setState({label: 'CLICKED'});
-      }.bind(this));
+    }).then((response) => {
+      this.setState({ list: response.data });
+      this.setState({ label: 'CLICKED' });
+    });
   }
 
   render() {
-    return(
+    const { label, list } = this.state;
+    return (
       <div>
-        <div>{this.state.list.map((e, i) => <li key={i}>{e.link}</li>)}</div>
-        <button type="button" onClick={this.getDataFromApi}>{this.state.label}</button>
+        <div>{ list.map((e) => <li key={e.id}>{ e.link }</li>) }</div>
+        <button type="button" onClick={this.getDataFromApi}>{ label }</button>
       </div>
-    )
+    );
   }
 }
 
