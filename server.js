d9b9150bf0a151a0c469bed3b5facc55729e5b2d
Indi <indiana.pinsard@wattys-conseil.com>
2021-03-02 12:09:51 +0100

    add express server

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.listen(port, () => console.log(`App is live on port ${port}!`));
+app.listen(port, () => `App is live on port ${port}!`);
